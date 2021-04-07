252749e2de2956aedebdc4402c23214f80f34382
Pavin Mathew <pvmathew@ucdavis.edu>
2021-04-07 16:22:01 +0900

    Keep running tab of number of questions and display upon quiz completion

--- src/components/Results.jsx
+++ src/components/Results.jsx
@@ -1,11 +1,41 @@
+/* eslint-disable react/jsx-one-expression-per-line */
 import React from 'react';
-import { Segment, Header, Button } from 'semantic-ui-react';
+import { useSelector } from 'react-redux';
+import { Segment, Header, Button, List, Container } from 'semantic-ui-react';
 
-const Results = () => (
-  <Segment textAlign='center'>
-    <Header as='h1'>The Quiz is now over!</Header>
-    <Button>View Source Code</Button>
-  </Segment>
-);
+const Results = () => {
+  const { numCorrect, numWrong } = useSelector(
+    (state) => state.questionsReducer
+  );
+
+  return (
+    <Segment textAlign='center'>
+      <Header as='h1'>The Quiz is now over!</Header>
+      <List divided verticalAlign='middle' textAlign='left'>
+        <List.Item>
+          <List.Content>
+            <List.Header>
+              Total Number of Questions: {numCorrect + numWrong}
+            </List.Header>
+          </List.Content>
+        </List.Item>
+        <List.Item>
+          <List.Content>
+            <List.Header>
+              Correct: <span style={{ color: 'green' }}>{numCorrect}</span>
+            </List.Header>
+          </List.Content>
+        </List.Item>
+        <List.Item>
+          <List.Content>
+            <List.Header>
+              Wrong: <span style={{ color: 'red' }}>{numWrong}</span>
+            </List.Header>
+          </List.Content>
+        </List.Item>
+      </List>
+    </Segment>
+  );
+};
 
 export default Results;
--- src/reducers/questions.js
+++ src/reducers/questions.js
@@ -1,9 +1,19 @@
-import { SET_QUESTIONS } from '../constants';
+import {
+  ANSWERED_CORRECTLY,
+  ANSWERED_INCORRECTLY,
+  SET_QUESTIONS,
+} from '../constants';
 
-const initialState = { questions: [] };
+const initialState = { questions: [], numCorrect: 0, numWrong: 0 };
 
 export default function questionsReducer(state = initialState, action) {
   switch (action.type) {
+    case ANSWERED_CORRECTLY: {
+      return { ...state, numCorrect: state.numCorrect + 1 };
+    }
+    case ANSWERED_INCORRECTLY: {
+      return { ...state, numWrong: state.numWrong + 1 };
+    }
     case SET_QUESTIONS:
       return {
         ...state,
